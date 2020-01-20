fef49d938c98286e14850a16ba4bf84d6143cfd0
anmasterova <55835760+anmasterova@users.noreply.github.com>
2020-01-21 02:27:44 +0300

    Add files via upload

---
+++ 05/serializer.h
@@ -0,0 +1,135 @@
+#include <sstream>
+
+
+enum class Error
+{
+    NoError,
+    CorruptedArchive
+};
+
+
+
+class Serializer
+{
+    static constexpr char Separator = ' '; 
+
+public:
+
+    explicit Serializer(std::ostream& out) : out_(out) {}
+
+    template <class T>
+    Error save(T& object)
+    {
+        return object.serialize(*this);
+    }
+
+    template <class... ArgsT>
+    Error operator()(ArgsT&&... args)
+    {
+        return process(std::forward<ArgsT>(args)...);
+    }
+    
+private:
+    // process использует variadic templates
+    std::ostream& out_;
+
+    template <class T, class... ArgsT>
+    Error process(T&& arg, ArgsT&&... args)
+    {
+        write(arg);
+        return process(std::forward<ArgsT>(args)...);
+    }
+
+    template <class T>
+    Error process(T&& arg)
+    {
+        write(arg);
+        return Error::NoError;
+    }
+    
+    void write(uint64_t arg)
+    {
+        out_ << arg << Separator;
+    }
+
+    void write(bool arg)
+    {
+        out_ << (arg ? "true" : "false") << Separator;
+    }
+};
+
+
+
+class Deserializer
+{
+    static constexpr char Separator = ' ';
+
+public:
+    
+    explicit Deserializer(std::istream& in) : in_(in) {}
+
+    template <class T>
+    Error load(T& object)
+    {
+        return object.deserialize(*this);
+    }
+
+    template <class... ArgsT>
+    Error operator()(ArgsT&&... args)
+    {
+        return process(std::forward<ArgsT>(args)...);
+    }
+    
+private:
+    
+    std::istream& in_;
+
+    template <class T, class... ArgsT>
+    Error process(T&& value, ArgsT&&... args)
+    {
+        if (load(value) == Error::CorruptedArchive)
+            return Error::CorruptedArchive;
+        else
+            return process(std::forward<ArgsT>(args)...);
+    }
+
+    template <class T>
+    Error process(T&& value)
+    {
+        return load(value);
+    }
+    
+    Error load(uint64_t& value)
+    {
+        std::string text;
+        in_ >> text;
+
+        if (text.empty())  
+            return Error::CorruptedArchive;
+        
+        for (size_t i = 0; i < text.length(); i++)
+            if (!std::isdigit(text[i]))
+                return Error::CorruptedArchive;
+        value = std::stoull(text);
+        
+        return Error::NoError;
+    }
+
+    Error load(bool& value)
+    {
+        std::string text;            
+        in_ >> text;
+        
+        if (text.empty())  
+            return Error::CorruptedArchive;
+        
+        if (text == "true")
+            value = true;
+        else if (text == "false")
+            value = false;
+        else
+            return Error::CorruptedArchive;
+        
+        return Error::NoError;
+    }
+};
---
+++ 06/format.h
@@ -0,0 +1,52 @@
+﻿#include <vector>
+#include <string>
+#include <sstream>
+#include <cassert>
+#include <iostream>
+
+template<class T>
+std::string any2str(const T& value) {
+    std::stringstream ss;
+    ss << value;
+    return ss.str();
+}
+
+std::vector<std::string> parseArgs() {
+    return {};
+}
+
+template<class First, class... Rest>
+std::vector<std::string> parseArgs(First first, Rest... rest) {
+    auto args = parseArgs(rest...);
+    args.push_back(any2str(first));
+    return args;
+}
+
+template<class... Args>
+std::string format(std::string str, Args... input){
+    auto args = parseArgs(input...);
+    assert(args.size() == sizeof...(input));
+
+    int n = args.size();
+    std::stringstream ss(str), result;
+    std::string tmp;
+
+    char ltok = '{';
+    char rtok = '}';
+    while(std::getline(ss, tmp, ltok)){
+        result << tmp;
+        int index;
+        ss >> index;
+        if (index < 0)
+        	throw std::runtime_error ("invalid argument");
+        if(ss.peek() != rtok)
+            throw std::runtime_error ("error format");
+        else
+            ss.ignore();
+        if(index < n)
+            result << args[n - 1 - index];
+        else
+            throw std::runtime_error ("something is wrong");
+    }
+    return result.str();
+}
